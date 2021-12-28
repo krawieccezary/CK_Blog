26dda632c4354d75a4e78edeb03ad8dbff036f3f
Cezary Krawiec <web@cezarykrawiec.pl>
2021-12-28 13:14:32 +0100

    add SendGrid service to contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 
@@ -13,12 +13,79 @@ const FormValidateInfo = styled.small`
   margin-top: 0.5rem;
 `;
 
+const SubmittingMessage = styled.span`
+  display: block;
+  margin: 2rem 0;
+  padding: 1.5rem 2rem;
+  color: var(--color-white);
+  
+  &.success {
+    background-color: var(--color-success);
+  }
+
+  &.error {
+    background-color: var(--color-error);
+  }
+  
+  a {
+    color: var(--white);
+    text-decoration: none;
+  }
+`;
+
+const SpanHoneyPot = styled.input`
+  visibility: hidden;
+  height: 0;
+`;
+
 const ContactForm = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const [submitting, setSubmitting] = useState(false);
+  const [submittingMessage, setSubmittingMessage] = useState({status: null, message: null});
+  const [isSpan, setIsSpan] = useState(false);
+
+  const onSubmit = async data => {
+    if(!isSpan){
+      setSubmitting(true);
+      setSubmittingMessage({status: null, message: null});
+      const { email, message: body, name } = data;
+  
+      const errorMessage = `Oops! Something gone wrong! Maybe try later or send message to <a href='mailto:web@cezarykrawiec.pl'>web@cezarykrawiec.pl</a>`;
+   
+      try {
+        const response = await fetch(process.env.GATSBY_EMAIL_FUNCTION_URL, {
+          method: "post",
+          headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+          },
+          body: new URLSearchParams({ email, name, body }).toString(),
+        })
+  
+        if (response.status === 200) {
+          setSubmitting(false);
+          setSubmittingMessage({status: 'success', message: `${name}, thanks for the message! I will response soon.`});
+          reset();
+          
+        } else {
+          setSubmitting(false);
+          setSubmittingMessage({status: 'error', message: errorMessage});
+        }
+  
+      } catch (error) {
+        setSubmitting(false);
+        setSubmittingMessage({status: 'error', message: errorMessage});
+      };
+    }
+
+  }
+
+
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form 
+      onSubmit={handleSubmit(onSubmit)}
+      method="post"
+    >
       <FormField>
         <input type="text" {...register('name', { required: true })} placeholder="Name"/>
         {errors.name && <FormValidateInfo>Name is require</FormValidateInfo>}
@@ -29,9 +96,11 @@ const ContactForm = () => {
       </FormField>
       <FormField>
         <textarea name="message" {...register('message', { required: true })} placeholder="Message"></textarea>
-        {errors.message && <FormValidateInfo>This field is require</FormValidateInfo>}
+        {errors.message && <FormValidateInfo>Message is require</FormValidateInfo>}
       </FormField>
-      <input className="button" type="submit" value="Send" />
+      <SpanHoneyPot type="checkbox" name="question" value="1" onClick={() => setIsSpan(true)}/>
+      <input className="button" type="submit" value="Send" disabled={submitting} />
+      {submittingMessage.message && <SubmittingMessage className={submittingMessage.status} dangerouslySetInnerHTML={{ __html: submittingMessage.message}}></SubmittingMessage>}
     </form>
   )
 }
